a6b29cab319e581db2c8af7ffe85efc623a698ec
Sahir Hossain <sahirh88@gmail.com>
2023-06-08 17:26:00 -0400

    closure on transfername.js

--- transferName.js
+++ transferName.js
@@ -1,7 +1,10 @@
-let boxes = document.getElementsByClassName("warframe-box");
-for (let i = 0; i < boxes.length; i++){
-    boxes[i].addEventListener("click", saveName)
+function saveWarframeInfo(){
+    let boxes = document.getElementsByClassName("warframe-box");
+    for (let i = 0; i < boxes.length; i++){
+        boxes[i].addEventListener("click", saveName)
+    }
 }
+saveWarframeInfo();
 
 function saveName(obj){
     let box = obj.currentTarget;
